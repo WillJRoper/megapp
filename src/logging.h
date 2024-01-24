5fc87a35e102241664daa50cd89dc20d8ce5d1ae
wjr21 <wjr21@sussex.ac.uk>
2024-01-24 19:20:01 +0000

    Better documenting logger

--- src/logging.h
+++ src/logging.h
@@ -9,27 +9,76 @@
 #include <stdexcept>
 #include <string>
 
+// Log levels
 enum LogLevel { ERROR, LOG, VERBOSE };
 
+/**
+ * @brief The Logging class provides a simple mechanism for logging messages
+ * to the standard output.
+ *
+ * The class is designed to provide a simple interface for logging messages
+ * to the standard output. The implementation ensures proper formatting of
+ * messages and allows for filtering based on a specified log level.
+ *
+ * Usage:
+ * - Create an instance of the Logging class with the desired log level.
+ * - Use the `message` function to log a message to the standard output.
+ * - Use the `error` function to log an error message and throw an exception.
+ * - Use the `tic` and `toc` functions to measure the duration of an operation.
+ */
 class Logging {
+private:
+  LogLevel _level; ///< The specified log level threshold.
+  std::chrono::high_resolution_clock::time_point _tic; ///< Timer start point.
+  std::chrono::high_resolution_clock::time_point _toc; ///< Timer end point.
+  std::string error_message_; ///< The formatted error message.
+
 public:
+  /**
+   * @brief Constructor for the Logging class.
+   *
+   * @param level The desired log level for filtering log messages.
+   */
   Logging(LogLevel level) : _level(level) {}
 
-  // Variadic template function for logging a message with a specified level.
+  /**
+   * @brief Log a verbose message.
+   *
+   * @tparam Args Variadic template for message formatting.
+   *
+   * @param format The format string for the log message.
+   * @param args The arguments for message formatting.
+   */
   template <typename... Args> void v_message(const char *format, Args... args) {
     if (_level >= 2) {
       log(format, args...);
     }
   }
 
-  // Variadic template function for logging a log message with a specified
-  // level.
+  /**
+   * @brief Log a regular log message.
+   *
+   * @tparam Args Variadic template for message formatting.
+   *
+   * @param format The format string for the log message.
+   * @param args The arguments for message formatting.
+   */
   template <typename... Args> void message(const char *format, Args... args) {
     if (_level >= LOG) {
       log(format, args...);
     }
   }
 
+  /**
+   * @brief Log an error message and throw a runtime error.
+   *
+   * @tparam Args Variadic template for message formatting.
+   *
+   * @param msg The error message format string.
+   * @param args The arguments for message formatting.
+   *
+   * @throw std::runtime_error Thrown with the formatted error message.
+   */
   template <typename... Args> void error(const std::string &msg, Args... args) {
     char buffer[256]; // Adjust the buffer size as needed.
     snprintf(buffer, sizeof(buffer), msg.c_str(), args...);
@@ -39,8 +88,16 @@ public:
     throw std::runtime_error(error_message_);
   }
 
+  /**
+   * @brief Start measuring time.
+   */
   void tic() { _tic = std::chrono::high_resolution_clock::now(); }
 
+  /**
+   * @brief Stop measuring time, log the duration, and print the log message.
+   *
+   * @param message The message indicating the operation being measured.
+   */
   void toc(const char *message) {
     _toc = std::chrono::high_resolution_clock::now();
 
@@ -52,24 +109,36 @@ public:
   }
 
 private:
-  LogLevel _level;
-  std::chrono::high_resolution_clock::time_point _tic;
-  std::chrono::high_resolution_clock::time_point _toc;
-  std::string error_message_;
-
-  // Variadic template function for formatting and logging.
+  /**
+   * @brief Log a formatted message.
+   *
+   * @tparam Args Variadic template for message formatting.
+   *
+   * @param format The format string for the log message.
+   * @param args The arguments for message formatting.
+   */
   template <typename... Args> void log(const char *format, Args... args) {
     std::ostringstream oss;
     oss << " " << getStep() << "--> ";
     logMessage(oss, format, args...);
   }
 
-  std::string getStep() {
-    // Replace this with your MPI rank retrieval code.
-    return "[0000]";
-  }
+  /**
+   * @brief Get the current step of the simulation.
+   *
+   * @return The current step of the simulation.
+   */
+  std::string getStep() { return "[0000]"; }
 
-  // Variadic template function for formatting the message.
+  /**
+   * @brief Log a formatted message.
+   *
+   * @tparam Args Variadic template for message formatting.
+   *
+   * @param oss The output string stream to write to.
+   * @param format The format string for the log message.
+   * @param args The arguments for message formatting.
+   */
   template <typename... Args>
   void logMessage(std::ostringstream &oss, const char *format, Args... args) {
     char buffer[256]; // Adjust the buffer size as needed.
